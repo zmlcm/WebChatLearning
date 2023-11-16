537b2525e004ed77464faf161c513fbd6718660a
zmlcm <974930069@qq.com>
2023-11-16 21:36:41 +0800

    0.0.1

---
+++ src/main/java/com/wechatlearning/demo/wechatlearning/WeChatLearningApplication.java
@@ -0,0 +1,13 @@
+package com.wechatlearning.demo.wechatlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wechatlearning/demo/wechatlearning/WeChatLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechatlearning.demo.wechatlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
